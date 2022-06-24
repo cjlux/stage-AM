ae1cbbe606e67f528c6c0a51aa5337be1f4eeff5
Baptiste Roulliaux <broulliaux@bordeaux-inp.fr>
2022-06-24 15:30:07 +0200

    Removal of run time management in the IIDRE's subscriber

--- DataFusion/Baptiste-Karam/iidre_uwb_indoor_geoloc-master/src/subscriber.py
+++ DataFusion/Baptiste-Karam/iidre_uwb_indoor_geoloc-master/src/subscriber.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import time, sys
 import rospy
-# from tf import TransformBroadcaster
 
 from std_msgs.msg import String
 
@@ -10,35 +9,25 @@ class iidre_listner:
        and stores those data in a file Data_iidre_{year}_{month}_{day}_{hour}_{minutes}_{seconds}.txt
        registered in the tree where the code is executed.
     '''
-    def __init__(self, opened_log_file, duration=None, verbose=False):
+    def __init__(self, opened_log_file, verbose=False):
         '''Parameters :
                 opened_log_file : the file where the data are stored
-                duration : duration of the algorithm's execution
                 verbose : define if there will be messages printed in the terminal
            Functions :
            rospy.Subscriber takes in parameters : the topic on which it is registered
            as a subscriber, the given type of the messages and a function to write what it hears.
-           The current time is also collected to apply the duration you want.
         '''
         self.verbose = verbose
         self.log_file = opened_log_file
         self.subscriber = rospy.Subscriber("/chatter", String, self.callback)
-        # self.tfb = TransformBroadcaster()
-        self.duration = duration
-        self.t0 = time.time()
         print("instance of iidre_listen created...")
 
     def callback(self, data):
-        '''Verify if the time is up. If it is, it stops the execution, but if not,
-           it calls the function parsing to only consider the relevant information
+        '''It first calls the function parsing to only consider the relevant information
            of the message. Then, it writes a message in rospy.loginfo about the data
            it hears (when the verbose variable is set to True). Then, it writes the
            data in the file.
         '''
-        if self.duration :
-        	t = time.time() - self.t0
-        	if t > self.duration :
-        		sys.exit()
         # self.parsing(data)
         if self.verbose: rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
         self.log_file.write(str(data.data)+"\n")
@@ -86,25 +75,14 @@ class iidre_listner:
 
 if __name__ == '__main__':
 
-   import time, sys
-   import argparse
-
-   parser = argparse.ArgumentParser()
-   parser.add_argument('--duration', action="store", dest="duration", type=int,
-   			help="duration of the experimentation in seconds")
-
-   args = parser.parse_args()
-   duration = None
-   if args.duration :
-   	duration = args.duration
-   	print("duration : ", duration)
+   import time
 
    uniq_file_name = f"./Data_iidre_{time.strftime('%y-%m-%d_%H-%M-%S', time.localtime())}.txt"
    print(f"writing data in <{uniq_file_name}>")
 
    with open(uniq_file_name, "w") as f:
 
-        listner = iidre_listner(f, duration)
+        listner = iidre_listner(f)
 
         # In ROS, nodes are uniquely named. If two nodes with the same
         # name are launched, the previous one is kicked off. The
